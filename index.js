4343933df5b2567b0e5f91620ad33569c51f236f
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-09-05 12:31:48 +0200

    Finish excerise

---
+++ index.js
@@ -0,0 +1,28 @@
+var fs = require('fs'),
+    colors = require('colors'),
+    folders = [],
+    files = [],
+    dir = './folder_to_check';
+
+fs.readdir(dir, 'utf-8', function(err, data) {
+    if (err) throw err;
+    console.log(`\nContents of ${dir.substr(2).magenta}:\n`);
+    
+    data.forEach((item) => {
+        if (item.includes('.')) {
+            files.push(item);
+        } else {
+            folders.push(item);
+        }
+    });
+
+    folders.forEach((item) => {
+        console.log(item.cyan);
+    });
+
+    files.forEach((item) => {
+        console.log(item.green);
+    });
+
+    console.log(`\nThere are ${folders.length} folders and ${files.length} files in directory: ${dir.substr(2).magenta}.`.yellow);
+});
